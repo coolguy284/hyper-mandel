87354a91c6e8f4878c880b428a8495271838514c
coolguy284 <coolguy284v64@gmail.com>
2022-12-30 00:09:31 -0500

    split main.cpp includes into main.h
    Signed-off-by: coolguy284 <coolguy284v64@gmail.com>

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,4 @@
-#include <Windows.h>
-#include <CommCtrl.h>
-
-#include "../resources/icon.h"
-
-#include "consts.h"
-#include "globals.h"
-#include "wnd_proc.h"
+#include "main.h"
 
 // declare the extern globals
 
---
+++ src/main.h
@@ -0,0 +1,8 @@
+#include <Windows.h>
+#include <CommCtrl.h>
+
+#include "../resources/icon.h"
+
+#include "consts.h"
+#include "globals.h"
+#include "wnd_proc.h"
